46daecce550294583b0511e42ddf04f95e7c90a3
root <fezwect@fezwect.fezwect>
2021-09-14 20:47:24 +0500

    snipets

---
+++ lab00.c
@@ -0,0 +1,25 @@
+//snipets
+'.source.c':
+  'main function':
+    'prefix': 'main'
+    'body': '''
+    #include<stdio.h>
+
+    int main()
+    {
+      $1
+    }
+    '''
+
+  'printf function':
+    'prefix': 'printf'
+    'body': '''printf("$1", $2);
+    $3
+    '''
+
+
+  'scanf function':
+    'prefix': 'scanf'
+    'body': '''scanf("$1",%$2);
+      $3
+    '''
